59cffda445f120c03026f84c8dc0484da2001d2e
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-13 09:27:05 +0300

    javascript-edit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let userName=document.getElementById("name");
-let urlCHeckName= "http://localhost/test/api1.php?str="+userName;
 
-fetch(urlCHeckName)
-  .then(response => response.json())
-  .then(data => console.log(data));
+
+
+function getData(){
+    let userName=document.getElementById("name");
+    let urlCHeckName= "http://localhost/test/api1.php?str="+userName;
+
+    fetch(urlCHeckName)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
