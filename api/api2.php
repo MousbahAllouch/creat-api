f87f71cbc9a7ae73d117efdbe0baa0c8629745ea
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-13 09:52:58 +0300

    api-edit

--- api/api2.php
+++ api/api2.php
@@ -4,7 +4,14 @@ $a = $_POST["a"];
 $b = $_POST["b"];
 $c = $_POST["c"];
 
-$result = ($a*$a*$a)+($b*$c)+($a/$b);
-echo $result;
+$calc = ($a**3)+($b*$c)-($a/$b);
+
+if (b==0){
+    $calc="b is equal to 0"
+}
+$result=[
+    "calc" => $calc,
+]
+echo json_encode($result)
 
 ?>
