67a5c164e4e169e26fdf1f9d7f81b644ad864d8e
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-13 09:23:56 +0300

    connect-api-with-frontend

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let userName=document.getElementById("name");
+let urlCHeckName= "http://localhost/test/api1.php?str="+userName;
 
+fetch(urlCHeckName)
+  .then(response => response.json())
+  .then(data => console.log(data));
